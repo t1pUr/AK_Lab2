f3c84b781b1a24817e677e0bc4a12e3600eb39a8
t1pur <simplex1135@gmail.com>
2021-11-03 20:10:24 +0200

    add files

---
+++ calculator.h
@@ -0,0 +1,12 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator
+{
+	public:
+		Calculator();
+		int Add (double, double);
+		int Sub (double, double);
+};
+
+#endif//CALCULATOR_H
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "calculator.h"
+
+using namespace std;
+
+
+int main() {
+    cout << "Main start";
+    Calculator* calculator = new Calculator();
+    cout << "Main end";
+}
